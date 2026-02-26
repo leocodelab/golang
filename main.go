1c51d600e3d6cb2986f03baad61cf5691e074e72
Leo <1065168165@qq.com>
2026-02-26 16:49:22 +0800

    2026.02.26 设置标头

--- main.go
+++ main.go
@@ -5,17 +5,16 @@ import (
 	"net/http"
 )
 
-func HandleFunc(w http.ResponseWriter, r *http.Request) {
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprintln(w, "index")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprintln(w, "/about")
+		fmt.Fprintln(w, "Hello World")
 	} else {
-		fmt.Fprintln(w, "error")
+		fmt.Fprintln(w, "error page")
 	}
 }
 
 func main() {
-	http.HandleFunc("/", HandleFunc)
+	http.HandleFunc("/", handleFunc)
 	http.ListenAndServe(":3000", nil)
 }
